d1528604ca9342508b59c0d8571f316b4b8ff5ff
potados99 <potados99@gmail.com>
2023-06-06 12:08:11 +0900

    feat: 보더 윈도우를 앱 스위처에서 숨기기

--- FocusTimer/Features/Timer/BorderWindow.xaml.cs
+++ FocusTimer/Features/Timer/BorderWindow.xaml.cs
@@ -11,6 +11,25 @@ namespace FocusTimer.Features.Timer
         public BorderWindow()
         {
             InitializeComponent();
+
+            HideFromAppSwitcher();
+        }
+
+        private void HideFromAppSwitcher()
+        {
+            Window w = new()
+            {
+                Top = -100, // Location of new window is outside of visible part of screen
+                Left = -100,
+                Width = 1, // size of window is enough small to avoid its appearance at the beginning
+                Height = 1,
+
+                WindowStyle = WindowStyle.ToolWindow, // Set window style as ToolWindow to avoid its icon in AltTab 
+                ShowInTaskbar = false
+            }; // Create helper window
+            w.Show(); // We need to show window before set is as owner to our main window
+            this.Owner = w; // Okey, this will result to disappear icon for main window.
+            w.Hide(); // Hide helper window just in case
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
