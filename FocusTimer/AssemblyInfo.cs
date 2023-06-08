19f97e0f20bf2f6363ab9949f9764e705ef42c45
potados99 <potados99@gmail.com>
2023-06-08 18:45:35 +0900

    chore: 버저닝 완성

--- FocusTimer/AssemblyInfo.cs
+++ FocusTimer/AssemblyInfo.cs
@@ -1,4 +1,7 @@
+using System.Reflection;
 using System.Windows;
+
+[assembly: AssemblyVersion("1.0.*")]
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
