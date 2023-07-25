5a1f5376dca19f3ee37cbffecf73743b7c892352
potados99 <potados99@gmail.com>
2023-07-25 14:34:46 +0900

    fix: 다른 모니터에 떠있는 작업 표시줄도 skip

--- FocusTimer/Lib/WindowWatcher.cs
+++ FocusTimer/Lib/WindowWatcher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows.Threading;
 
@@ -10,15 +11,18 @@ namespace FocusTimer.Lib
 
         public event FocusedEventHandler? OnFocused;
 
-        private readonly DispatcherTimer Timer = new DispatcherTimer();
+        private readonly DispatcherTimer Timer = new();
 
         public static readonly string[] SkipList = new string[] {
                 "TaskListThumbnailWnd", // 윈도우가 여러 개일 때 작업표시줄에 표시되는 작은 썸네일
                 "ForegroundStaging", // Alt + Tab으로 보이는 UI
                 "MultitaskingViewFrame", // Alt + Tab으로 보이는 UI
                 "Windows.UI.Core.CoreWindow", // 시작 버튼 누르면 보이는 UI
-                "Shell_TrayWnd", // 작업표시줄 아이콘 우클릭하면 보이는 UI
-                "ApplicationManager_DesktopShellWindow" // 새 창 띄울 때에 개입하는 UI
+                "Shell_TrayWnd", // 작업표시줄
+                "Shell_SecondaryTrayWnd", // 다른 모니터에 떠있는 보조 작업표시줄
+                "ApplicationManager_DesktopShellWindow", // 새 창 띄울 때에 개입하는 UI
+                "WorkerW", // 작업표시줄 오른쪽 눌러서 나오는 바탕화면 UI
+                "NotifyIconOverflowWindow", // 작업표시줄에 아이콘이 몰려있는 곳 위로가는 쉐브론 누르면 나오는 UI
             };
 
         public IntPtr FocusedWindow { get; private set; } = IntPtr.Zero;
