6725ff1475495a6bab12a53a48a9671470a0a0d3
potados99 <potados99@gmail.com>
2023-08-21 13:47:41 +0900

    refactor: extract draggable code to base class

--- FocusTimer/Features/Timer/MainWindow.xaml.cs
+++ FocusTimer/Features/Timer/MainWindow.xaml.cs
@@ -164,67 +164,14 @@ public partial class MainWindow : MainViewModelWindow
         ViewModel.ToggleFocusLock();
     }
 
+    protected override void OnClick()
+    {
+        ViewModel.ToggleExpanded();
+    }
+
     #endregion
 }
 
-public abstract class MainViewModelWindow : BaseWindow<MainViewModel>
+public abstract class MainViewModelWindow : BaseDraggableWindow<MainViewModel>
 {
-    #region 드래그 오버라이드
-
-    Point prepoint;
-    bool isDragInProgress;
-    bool isMouseCaptured;
-    bool isDraggedFar;
-
-    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
-    {
-        isDragInProgress = true;
-        prepoint = Mouse.GetPosition(this);
-
-        CaptureMouse();
-        isMouseCaptured = true;
-
-        base.OnMouseLeftButtonDown(e);
-    }
-
-    protected override void OnMouseMove(MouseEventArgs e)
-    {
-        if (!isDragInProgress || !isMouseCaptured)
-        {
-            return;
-        }
-
-        Point newmouse = Mouse.GetPosition(this);
-        double top = (double) newmouse.Y - (double) prepoint.Y;
-        double left = (double) newmouse.X - (double) prepoint.X;
-
-        if (Math.Abs(top) > 0 || Math.Abs(left) > 0)
-        {
-            isDraggedFar = true;
-        }
-
-        SetValue(MainWindow.TopProperty, (Application.Current.MainWindow.Top + top));
-        SetValue(MainWindow.LeftProperty, (Application.Current.MainWindow.Left + left));
-
-        base.OnMouseMove(e);
-    }
-
-    protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
-    {
-        bool wasDraggedFar = isDraggedFar;
-
-        isDragInProgress = false;
-        isDraggedFar = false;
-        ReleaseMouseCapture();
-        isMouseCaptured = false;
-
-        if (!wasDraggedFar)
-        {
-            base.OnMouseLeftButtonUp(e);
-
-            ViewModel.ToggleExpanded();
-        }
-    }
-
-    #endregion
 }
---
+++ FocusTimer/Lib/Component/BaseDraggableWindow.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using FocusTimer.Features.Timer;
+
+namespace FocusTimer.Lib.Component;
+
+public abstract class BaseDraggableWindow<T> : BaseWindow<T> where T : BaseViewModel
+{
+    #region 드래그 오버라이드
+
+    Point prepoint;
+    bool isDragInProgress;
+    bool isMouseCaptured;
+    bool isDraggedFar;
+
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        isDragInProgress = true;
+        prepoint = Mouse.GetPosition(this);
+
+        CaptureMouse();
+        isMouseCaptured = true;
+
+        base.OnMouseLeftButtonDown(e);
+    }
+
+    protected override void OnMouseMove(MouseEventArgs e)
+    {
+        if (!isDragInProgress || !isMouseCaptured)
+        {
+            return;
+        }
+
+        Point newmouse = Mouse.GetPosition(this);
+        double top = (double) newmouse.Y - (double) prepoint.Y;
+        double left = (double) newmouse.X - (double) prepoint.X;
+
+        if (Math.Abs(top) > 0 || Math.Abs(left) > 0)
+        {
+            isDraggedFar = true;
+        }
+
+        SetValue(MainWindow.TopProperty, Top + top);
+        SetValue(MainWindow.LeftProperty, Left + left);
+
+        base.OnMouseMove(e);
+    }
+
+    protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+    {
+        bool wasDraggedFar = isDraggedFar;
+
+        isDragInProgress = false;
+        isDraggedFar = false;
+        ReleaseMouseCapture();
+        isMouseCaptured = false;
+
+        if (!wasDraggedFar)
+        {
+            base.OnMouseLeftButtonUp(e);
+
+            OnClick();
+        }
+    }
+    
+    protected virtual void OnClick() {}
+
+    #endregion
+}
