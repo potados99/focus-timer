03067945d68d6eeaf88aa22efd7c6797015d5e1b
potados99 <potados99@gmail.com>
2023-10-14 15:44:12 +0900

    chore: 철회

--- FocusTimer/Features/Timer/Slot/TimerSlotViewModel.cs
+++ FocusTimer/Features/Timer/Slot/TimerSlotViewModel.cs
@@ -14,6 +14,7 @@
 // 라이센스 전문은 이 프로그램과 함께 제공되었을 것입니다. 만약 아니라면,
 // 다음 링크에서 받아볼 수 있습니다: <https://www.gnu.org/licenses/gpl-3.0.txt>
 
+using System.Diagnostics.CodeAnalysis;
 using FocusTimer.Domain.Services;
 using FocusTimer.Library;
 using FocusTimer.Library.Extensions;
@@ -57,6 +58,7 @@ public partial class TimerSlotViewModel
         _eventService.OnRender += OnRender;
     }
     
+    [SuppressMessage("ReSharper.DPA", "DPA0000: DPA issues")]
     private void LoadSlot()
     {
         this.GetLogger().Info($"현재 슬롯({SlotNumber}번)의 앱 정보를 불러옵니다.");
